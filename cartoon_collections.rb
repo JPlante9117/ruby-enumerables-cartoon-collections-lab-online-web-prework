b1e382edc08308894c6af64db62d978c7653b663
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 18:29:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(array)
-  array.each_with_index{|dwarf| puts dwarf}
+  array.each_with_index{|dwarf, num| 
+  hash[]
+  puts num dwarf}
 end
 
 def summon_captain_planet# code an argument here
