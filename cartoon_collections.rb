540542f0e8d8bdf31c0389084b3d088d6398d2d7
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-09-06 19:12:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,6 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  array.map.find("cheese") {|cheese| "#{cheese}"}
+  array.map.detect ("cheese") {|cheese| "#{cheese}"}
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
